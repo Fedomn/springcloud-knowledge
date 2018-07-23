f91845a0730b65d966895858991150f119a9761c
Fedomn <shadowforfiend@gmail.com>
2018-07-23 13:28:16 +0800

    add feign ConsumerPactVerificationTest

--- feign-service/src/main/java/com/fedomn/feignservice/FeignServiceApplication.java
+++ feign-service/src/main/java/com/fedomn/feignservice/FeignServiceApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -17,7 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FeignServiceApplication {
 
-  @Autowired private ComputeClient computeClient;
+  private final ComputeClient computeClient;
+
+  @Autowired
+  public FeignServiceApplication(ComputeClient computeClient) {
+    this.computeClient = computeClient;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(FeignServiceApplication.class, args);
@@ -26,12 +28,5 @@ public class FeignServiceApplication {
   @GetMapping("/add")
   public Integer add() {
     return computeClient.add(10, 20);
-  }
-
-  @Component
-  @FeignClient("compute-service")
-  interface ComputeClient {
-    @GetMapping("add")
-    Integer add(@RequestParam Integer a, @RequestParam Integer b);
   }
 }
---
+++ feign-service/src/test/java/ConsumerPactVerificationTest.java
@@ -0,0 +1,53 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.PactProviderRuleMk2;
+import au.com.dius.pact.consumer.PactVerification;
+import au.com.dius.pact.consumer.dsl.PactDslJsonRootValue;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.RequestResponsePact;
+import com.fedomn.feignservice.ComputeClient;
+import com.fedomn.feignservice.FeignServiceApplication;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+    properties = {
+      "eureka.client.enabled: false",
+      "compute-service.ribbon.listOfServers: localhost:8181",
+    },
+    classes = FeignServiceApplication.class)
+public class ConsumerPactVerificationTest {
+
+  @Rule
+  public PactProviderRuleMk2 stubProvider =
+      new PactProviderRuleMk2("compute-service", "localhost", 8181, this);
+
+  @Autowired private ComputeClient computeClient;
+
+  @Pact(state = "provider two numbers", provider = "compute-service", consumer = "computeClient")
+  public RequestResponsePact createAddPact(PactDslWithProvider builder) {
+    return builder
+        .given("provider two numbers")
+        .uponReceiving("a request to GET the sum of two numbers")
+        .path("/add")
+        .query("a=1&b=2")
+        .method("GET")
+        .willRespondWith()
+        .status(200)
+        .body(PactDslJsonRootValue.integerType(3))
+        .toPact();
+  }
+
+  @Test
+  @PactVerification(fragment = "createAddPact")
+  public void verifyCreatePersonPact() {
+    Integer addResult = computeClient.add(1, 2);
+    assertThat(addResult).isEqualTo(3);
+  }
+}
