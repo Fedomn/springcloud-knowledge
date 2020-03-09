853f72b9e17a57ae4c202fc6a3aa8aaeb0c2a8d4
Fedomn <shadowforfiend@gmail.com>
2020-03-09 11:14:45 +0800

    testing for eureka client in fargate.

---
+++ eureka-server/src/main/java/com/fedomn/eurekaserver/aws/Converter.java
@@ -0,0 +1,39 @@
+package com.fedomn.eurekaserver.aws;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import java.io.IOException;
+
+public class Converter {
+
+  // Serialize/deserialize helpers
+
+  private static ObjectReader reader;
+  private static ObjectWriter writer;
+
+  public static EcsTaskMetadata fromJsonString(String json) throws IOException {
+    return getObjectReader().readValue(json);
+  }
+
+  public static String toJsonString(EcsTaskMetadata obj) throws JsonProcessingException {
+    return getObjectWriter().writeValueAsString(obj);
+  }
+
+  private static void instantiateMapper() {
+    ObjectMapper mapper = new ObjectMapper();
+    reader = mapper.reader(EcsTaskMetadata.class);
+    writer = mapper.writerFor(EcsTaskMetadata.class);
+  }
+
+  private static ObjectReader getObjectReader() {
+    if (reader == null) instantiateMapper();
+    return reader;
+  }
+
+  private static ObjectWriter getObjectWriter() {
+    if (writer == null) instantiateMapper();
+    return writer;
+  }
+}
